42448974cfb8d1a21c80fdc1ac47ab2e0955eb72
Anna Dereniuk <Anna_Dereniuk@epam.com>
2019-02-25 16:34:24 +0200

    Program gets 5 numbers from console, multiplies them by 10 and outputs results

---
+++ InputProcessing/InputProcessing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InputProcessing
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] value = new int[5];
+            string input;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter Value № {0}", (i + 1));
+                input = Console.ReadLine();
+                value[i] = Convert.ToInt32(input);
+            }
+            Console.WriteLine("Your input is");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(value[i]);
+            }
+            Console.WriteLine("Your output is");
+            for (int i = 0; i < 5; i++)
+            {
+                value[i] = value[i] * 10;
+                Console.WriteLine(value[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
